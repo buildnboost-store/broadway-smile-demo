3dec11f53aecfa7fd729967e8823a7a6813593a1
buildnboost-store <buildnboost.store@gmail.com>
2026-02-16 21:10:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-})
+  root: path.resolve(__dirname, "client"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client", "src"),
+      "@assets": path.resolve(__dirname, "client", "src", "assets"),
+      "@shared": path.resolve(__dirname, "shared"),
+    },
+  },
+});
